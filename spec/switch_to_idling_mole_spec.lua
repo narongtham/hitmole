13352d433a0fa683c66760548d0da73ba048540a
Jack Swallow <ninjaadvance@gmail.com>
2014-01-16 15:53:39 +0700

    Add switchToIdlingMole

---
+++ spec/switch_to_idling_mole_spec.lua
@@ -0,0 +1,28 @@
+describe("SwitchToIdlingMole spec", function ( ... )
+
+	local switchToIdlingMole = require( "switchToIdlingMole" )
+
+	it("Set mole sprite sequence to 'idle'.", function ( ... )
+		error( "Not yet implemented." )
+	end)
+
+	it("Play sprite after set sequence", function ( ... )
+		error( "Not yet implemented." )
+	end)
+
+	it("Bind tap event listener to sprite", function ( ... )
+		error( "Not yet implemented." )
+	end)
+
+	it("Start timer for make mole escape", function ( ... )
+		error( "Not yet implemented" )
+	end)
+
+	it("When sprite tapped .It should switch mole to dying state.", function ( ... )
+		error( "Not yet implemented." )
+	end)
+
+	it("When timed out. It should switch mole to escaping state.", function ( ... )
+		error( "Not yet implemented" )
+	end)
+end)
---
+++ switchToIdlingMole.lua
@@ -0,0 +1,3 @@
+local switchToIdlingMole = {}
+
+return switchToIdlingMole
