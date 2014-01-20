d6084de33418487ab21bb2bfff2d4169df2ef49e
Jack Swallow <ninjaadvance@gmail.com>
2014-01-20 12:30:03 +0700

    Remove life indicator after decrease lifepoint

--- decreaseLifePoint.lua
+++ decreaseLifePoint.lua
@@ -2,6 +2,7 @@ decreaseLifePoint = {}
 
 function decreaseLifePoint.decreaseByOne()
 	escapeCount = escapeCount-1
+	display.remove( heart[escapeCount] )
 end
 
 return decreaseLifePoint
--- spec/decrease_life_point_spec.lua
+++ spec/decrease_life_point_spec.lua
@@ -3,6 +3,7 @@ describe("DecreaseLifePoint spec", function ( ... )
 	local decreaseLifePoint = require "decreaseLifePoint"
 
 	display = {}
+	heart = { {}, {}, {}}
 	escapeCount = 3
 
 	it("It should decrease escape count.", function ( ... )
@@ -13,5 +14,14 @@ describe("DecreaseLifePoint spec", function ( ... )
 		--then
 		--assert.stub(display.remove).was_called_with(heart[escapeCount])
 		assert.are.equal(escapeCount, 2)
+	end)
+
+	it("It should remove one lifepoint indicator from remaining lifepoint indicators", function ( ... )
+		--given
+		stub(display, "remove")
+		--when
+		decreaseLifePoint.decreaseByOne()
+		--then
+		assert.stub(display.remove).was_called_with(heart[escapeCount])
 	end)
 end)
