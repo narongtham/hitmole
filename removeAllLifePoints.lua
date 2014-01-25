dd795665339f49fb3f432196dc55f51b929f9a9b
Jack Swallow <ninjaadvance@gmail.com>
2014-01-25 22:40:50 +0700

    Make code more cleaner in removeAllLifePoints

--- removeAllLifePoints.lua
+++ removeAllLifePoints.lua
@@ -1,11 +1,10 @@
 removeAllLifePoints = {}
 
 function removeAllLifePoints.evaluate()
-	for i = 1, escapeCount do
-		display.remove( lifePoints[i] )
+	while escapeCount > 0 do
+		display.remove( lifePoints[escapeCount] )
+		escapeCount = escapeCount-1
 	end
-
-	escapeCount = 0
 end
 
 return removeAllLifePoints
