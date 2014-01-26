64e4a6453b39124df2ba20a549d8090c7325f579
Jack Swallow <ninjaadvance@gmail.com>
2014-01-26 20:15:05 +0700

    Set x,y position to decorator image

---
+++ createDecorator.lua
@@ -0,0 +1,9 @@
+createDecorator = {}
+
+function createDecorator.evaluate()
+	local decoratorImage = display.newImage( "img/sprite/rubber_tube.png" )
+	decoratorImage.x = 90
+	decoratorImage.y = 320
+end
+
+return createDecorator
---
+++ spec/create_decorator_spec.lua
@@ -0,0 +1,41 @@
+describe("createDecorator", function ( ... )
+
+	local createDecorator = require "createDecorator"
+
+	local decoratorImage
+
+	setup(function ( ... )
+		decoratorImage = {}
+
+		display.newImage = function ( ... )
+			return decoratorImage
+		end
+	end)
+
+	it("Create new decorator image", function ( ... )
+		-- given
+		spy.on(display, "newImage")
+		-- when
+		createDecorator.evaluate()
+		-- then
+		assert.spy(display.newImage).was_called_with("img/sprite/rubber_tube.png")
+	end)
+
+	it("Set x position to 90", function ( ... )
+		-- given
+		local expected_x = 90
+		-- when
+		createDecorator.evaluate()
+		--then
+		assert.are.equal(decoratorImage.x, expected_x)
+	end)
+
+	it("Set y position to 320", function ( ... )
+		-- given
+		local expected_y = 320
+		-- when
+		createDecorator.evaluate()
+		--then
+		assert.are.equal(decoratorImage.y, expected_y)
+	end)
+end)
