bbc2447a33cc4341903a035e6f310f3941b92e8d
Jack Swallow <ninjaadvance@gmail.com>
2014-01-31 20:20:20 +0700

    Refactor module verifyMoleRegions

--- verifyMoleRegion.lua
+++ verifyMoleRegion.lua
@@ -1,45 +1,48 @@
 verifyMoleRegions = {}
 
 function verifyMoleRegions.evaluate(generatePosition, targetMole)
-	local targetMoleMetadata = {
+	verifyMoleRegions.verifyOnActualRegion(generatePosition, targetMole)
+	verifyMoleRegions.verifyOnLeftRegion(generatePosition, targetMole)
+	verifyMoleRegions.verifyOnRightRegion(generatePosition, targetMole)
+	verifyMoleRegions.verifyOnTopRegion(generatePosition, targetMole)
+	verifyMoleRegions.verifyOnBottomRegion(generatePosition, targetMole)
+end
+
+function verifyMoleRegions.createMoleMetadata(targetMole)
+	return {
 		x = targetMole.x,
 		y = targetMole.y,
 		contentWidth = targetMole.contentWidth,
 		contentHeight = targetMole.contentHeight
 	}
+end
 
+function verifyMoleRegions.verifyOnActualRegion(generatePosition, targetMole)
+	local targetMoleMetadata = verifyMoleRegions.createMoleMetadata(targetMole)
 	isMolePositionIntersect.evaluate(generatePosition, targetMoleMetadata)
+end
 
-	local leftMoleMetadata = {
-		x = targetMole.x-(targetMole.contentWidth/2),
-		y = targetMole.y,
-		contentWidth = targetMole.contentWidth,
-		contentHeight = targetMole.contentHeight
-	}
+function verifyMoleRegions.verifyOnLeftRegion(generatePosition, targetMole)
+	local leftMoleMetadata = verifyMoleRegions.createMoleMetadata(targetMole)
+	leftMoleMetadata.x = targetMole.x-(targetMole.contentWidth/2),
 	isMolePositionIntersect.evaluate(generatePosition, leftMoleMetadata)
+end
 
-	local rightMoleMetadata = {
-		x = targetMole.x+(targetMole.contentWidth/2),
-		y = targetMole.y,
-		contentWidth = targetMole.contentWidth,
-		contentHeight = targetMole.contentHeight
-	}
+function verifyMoleRegions.verifyOnRightRegion(generatePosition, targetMole)
+	local rightMoleMetadata = verifyMoleRegions.createMoleMetadata(targetMole)
+	rightMoleMetadata.x = targetMole.x+(targetMole.contentWidth/2),
 	isMolePositionIntersect.evaluate(generatePosition, rightMoleMetadata)
+end
 
-	local topMoleMetadata = {
-		x = targetMole.x,
-		y = targetMole.y-(targetMole.contentHeight/2),
-		contentWidth = targetMole.contentWidth,
-		contentHeight = targetMole.contentHeight
-	}
+function verifyMoleRegions.verifyOnTopRegion(generatePosition, targetMole)
+	local topMoleMetadata = verifyMoleRegions.createMoleMetadata(targetMole)
+	topMoleMetadata.y = targetMole.y-(targetMole.contentHeight/2)
 	isMolePositionIntersect.evaluate(generatePosition, topMoleMetadata)
+end
 
-	local bottomMoleMetadata = {
-		x = targetMole.x,
-		y = targetMole.y+(targetMole.contentHeight/2),
-		contentWidth = targetMole.contentWidth,
-		contentHeight = targetMole.contentHeight
-	}
+function verifyMoleRegions.verifyOnBottomRegion(generatePosition, targetMole)
+	local bottomMoleMetadata = verifyMoleRegions.createMoleMetadata(targetMole)
+	bottomMoleMetadata.y = targetMole.y+(targetMole.contentHeight/2)
 	isMolePositionIntersect.evaluate(generatePosition, bottomMoleMetadata)
 end
 
