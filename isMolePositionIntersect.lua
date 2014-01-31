d15d6710e81ae1b4d97f71f9384a0ede4f7a3d9c
Jack Swallow <ninjaadvance@gmail.com>
2014-01-31 17:14:37 +0700

    Refactor isMolePositionIntersect more readable

--- isMolePositionIntersect.lua
+++ isMolePositionIntersect.lua
@@ -5,8 +5,15 @@ function isMolePositionIntersect.evaluate(generatedPosition, target)
 		error("Detected intersect position")
 	end
 
-	if generatedPosition.x >= target.x - (target.contentWidth/2) and generatedPosition.x <= target.x + (target.contentWidth/2) then
-		if generatedPosition.y >= target.y - (target.contentHeight/2) and generatedPosition.y <= target.y +(target.contentHeight/2) then
+	local rangeWidth = (target.contentWidth/2)
+	local rangeHeight = (target.contentHeight/2)
+	local leftMost = target.x - rangeWidth
+	local rightMost = target.x + rangeWidth
+	local topMost = target.y - rangeHeight
+	local bottomMost = target.y + rangeHeight
+
+	if generatedPosition.x >= leftMost and generatedPosition.x <= rightMost then
+		if generatedPosition.y >= topMost and generatedPosition.y <= bottomMost then
 			error("Detected intersect position")
 		end
 	end
