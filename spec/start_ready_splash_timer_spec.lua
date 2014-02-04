325ba6ca64e4f9af155b5282175124e16348a0a7
Jack Swallow <ninjaadvance@gmail.com>
2014-02-04 12:45:47 +0700

    Remove ready splash when timer end

--- spec/start_ready_splash_timer_spec.lua
+++ spec/start_ready_splash_timer_spec.lua
@@ -2,8 +2,12 @@ describe("startReadySplashTimer", function ( ... )
 	local startReadySplashTimer
 
 	timer = {}
-
+	local fakeTimerInstance
+	local target = {}
 	setup(function ( ... )
+
+		fakeTimerInstance = {}
+
 		timer.performWithDelay = function ( ... )
 			return fakeTimerInstance
 		end
@@ -19,8 +23,15 @@ describe("startReadySplashTimer", function ( ... )
 
 	it("Start timer with proper parameters", function ( ... )
 		-- when
-		startReadySplashTimer.evaluate()
+		startReadySplashTimer.evaluate(target)
 		-- then
 		assert.spy(timer.performWithDelay).was_called_with(1000, onReadySplashTimerEnded.evaluate)
+	end)
+
+	it("Set target to timer's instance parameter", function ( ... )
+		-- when
+		startReadySplashTimer.evaluate(target)
+		-- then
+		assert.are.equal(fakeTimerInstance.source.parameters.target, target)
 	end)
 end)
