0054db3df7ee27558b2de838dc546207d493f050
Jack Swallow <ninjaadvance@gmail.com>
2014-02-04 20:47:36 +0700

    Update showReadySplash to take second arguments as sequenceImageData

--- showReadySplash.lua
+++ showReadySplash.lua
@@ -2,8 +2,8 @@ showReadySplash = {}
 
 startReadySplashTimer = startReadySplashTimer or require("startReadySplashTimer")
 
-function showReadySplash.evaluate(group)
-	local splashImage = display.newImage( "img/sprite/ready/go.png" )
+function showReadySplash.evaluate(group, splashImageSequenceData)
+	local splashImage = display.newImage( splashImageSequenceData.value )
 	splashImage.x = DISPLAY_CENTER_X
 	splashImage.y = DISPLAY_CENTER_Y
 	group:insert(splashImage)
--- spec/show_ready_splash_spec.lua
+++ spec/show_ready_splash_spec.lua
@@ -3,6 +3,9 @@ describe("showReadySplash", function ( ... )
 
 	local splashImage
 	local group
+	local splashImageSequenceData = {
+		value = "img/sprite/ready/go.png"
+	}
 
 	setup(function ( ... )
 		splashImage = {}
@@ -25,16 +28,16 @@ describe("showReadySplash", function ( ... )
 		--given
 		spy.on(display, "newImage")
 		--when
-		showReadySplash.evaluate(group)
+		showReadySplash.evaluate(group, splashImageSequenceData)
 		--then
-		assert.spy(display.newImage).was_called_with("img/sprite/ready/go.png")
+		assert.spy(display.newImage).was_called_with(splashImageSequenceData.value)
 	end)
 
 	it("Set image x position to center", function ( ... )
 		-- given
 		DISPLAY_CENTER_X = 100
 		--when
-		showReadySplash.evaluate(group)
+		showReadySplash.evaluate(group, splashImageSequenceData)
 		--then
 		assert.are.equal(splashImage.x, DISPLAY_CENTER_X)
 	end)
@@ -43,7 +46,7 @@ describe("showReadySplash", function ( ... )
 		-- given
 		DISPLAY_CENTER_Y = 120
 		--when
-		showReadySplash.evaluate(group)
+		showReadySplash.evaluate(group, splashImageSequenceData)
 		--then
 		assert.are.equal(splashImage.y, DISPLAY_CENTER_Y)
 	end)
@@ -52,14 +55,14 @@ describe("showReadySplash", function ( ... )
 		--given
 		stub(group, "insert")
 		--when
-		showReadySplash.evaluate(group)
+		showReadySplash.evaluate(group, splashImageSequenceData)
 		--then
 		assert.stub(group.insert).was_called_with(group, splashImage)
 	end)
 
 	it("Evaluate startSplashScreenTimer", function ( ... )
 		-- when
-		showReadySplash.evaluate(group)
+		showReadySplash.evaluate(group, splashImageSequenceData)
 		-- then
 		assert.stub(startReadySplashTimer.evaluate).was_called_with(splashImage)
 	end)
