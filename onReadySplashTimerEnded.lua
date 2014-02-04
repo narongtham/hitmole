0e2637e2fbcb06bb1e4d597c33edaa1855950449
Jack Swallow <ninjaadvance@gmail.com>
2014-02-04 21:50:25 +0700

    Update onReadySplashTimerEnded, add condition for support sequence

--- onReadySplashTimerEnded.lua
+++ onReadySplashTimerEnded.lua
@@ -1,10 +1,18 @@
 onReadySplashtimerEnded = {}
 
 startGenerateMoleTimer = startGenerateMoleTimer or require("startGenerateMoleTimer")
+showReadySplash = showReadySplash or require("showReadySplash")
 
 function onReadySplashtimerEnded.evaluate(event)
 	display.remove(event.source.params.target)
-	startGenerateMoleTimer.evaluate()
+
+	local imageSequenceData = event.source.params.imageSequenceData
+
+	if imageSequenceData.next then
+		showReadySplash.evaluate(currentViewGroup, imageSequenceData.next)
+	else
+		startGenerateMoleTimer.evaluate()
+	end
 end
 
 return onReadySplashtimerEnded
--- spec/on_ready_splash_timer_spec.lua
+++ spec/on_ready_splash_timer_spec.lua
@@ -2,6 +2,9 @@ describe("onReadySplashTimerEnded", function ( ... )
 	local onReadySplashTimerEnded
 	local event
 	local target = {}
+	local splashImageSequenceData = {
+		value = "img/sprite/ready/go.png"
+	}
 
 	setup(function ( ... )
 		display = {}
@@ -10,13 +13,17 @@ describe("onReadySplashTimerEnded", function ( ... )
 		event = {
 			source = {
 				params = {
-					target = target
+					target = target,
+					imageSequenceData = splashImageSequenceData
 				}
 			}
 		}
 
 		startGenerateMoleTimer = {}
 		stub(startGenerateMoleTimer, "evaluate")
+
+		showReadySplash = {}
+		stub(showReadySplash, "evaluate")
 
 		onReadySplashTimerEnded = require("onReadySplashTimerEnded")
 	end)
@@ -28,10 +35,22 @@ describe("onReadySplashTimerEnded", function ( ... )
 		assert.stub(display.remove).was_called_with(target)
 	end)
 
-	it("Start generate mole timer", function ( ... )
+	it("Start generate mole timer when no next sequence", function ( ... )
+		-- given
+		splashImageSequenceData.next = nil
 		-- when
 		onReadySplashTimerEnded.evaluate(event)
 		-- then
 		assert.stub(startGenerateMoleTimer.evaluate).was_called()
+	end)
+
+	it("Evaluate showReadySplash with next sequence if available.", function ( ... )
+		-- given
+		currentViewGroup = {}
+		splashImageSequenceData.next = {}
+		-- when
+		onReadySplashTimerEnded.evaluate(event)
+		-- then
+		assert.stub(showReadySplash.evaluate).was_called_with(currentViewGroup, splashImageSequenceData.next)
 	end)
 end)
