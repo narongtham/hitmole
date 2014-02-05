2ab78fb7c9803f77cbc768c97ac5f5523c890799
Narongtham <apple@apples-Mac-mini.local>
2014-02-05 13:53:08 +0700

    optimize LV table and fix 3 2 1

--- config/readySplashConfigurations.lua
+++ config/readySplashConfigurations.lua
@@ -1,9 +1,9 @@
 readySplashConfigurations = {
 	sequence = {
 		"img/sprite/ready/go.png",
-		"img/sprite/ready/3.png",
+		"img/sprite/ready/1.png",
 		"img/sprite/ready/2.png",
-		"img/sprite/ready/1.png"
+		"img/sprite/ready/3.png"
 	},
 	delay = 1000
 }
